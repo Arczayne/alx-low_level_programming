50c40996f422730e8508072bdf86a0f69ca7be3d
Zayne <arczayne@gmail.com>
2022-06-15 20:44:46 -0700

    Straight line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,14 +10,14 @@ void print_line(int n)
 {
 	int i;
 
-	for (i = 1; i <= n; i++)	
+	for (i = 1; i <= n; i++)
 
 		if (i == 0 && i < 0)
 		{
 			_putchar('\n');
 		}
 		else
-		{
+		}
 			_putchar('_');
 		}
 			_putchar('\n');
