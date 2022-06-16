4673f5b08a99ca82186032d6ff9b14780957debc
Zayne <arczayne@gmail.com>
2022-06-15 21:58:51 -0700

    Reverse string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * print_rev - prints the revers of a string
+ * @s: is the string
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	char i;
+
+
+	for (i = 0; i <= 9; i--)
+	{
+		_putchar(s[i] + '0');
+	}
+
+	_putchar('\n');
+}
